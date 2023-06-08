639a2d0ad26c26d3cbeb61c76d20c8aa2486b6f4
AlexsandroAmancio85 <alexsandroamancio85@gmail.com>
2023-06-08 09:38:49 -0300

    Estrutura HTML

---
+++ scripts/script.js
@@ -0,0 +1,6 @@
+
+// Carne - 400 gr por pessoa + de 6 horas - 650 gr
+// Cerveja - 1200 ml por pessoa + de 6 horas - 2000 ml
+// Refrigerante/água - 1000 ml por pessoa + de 6 horas 1500 ml
+// crianças valem 0,5
+
